bd50d87f69cb2232b2220c24483a8c5c5af5e99f
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-03-03 08:46:04 +0800

    feat: nested routes

---
+++ src/app/blog/nested/page.tsx
@@ -0,0 +1,7 @@
+export default function Nested() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl">Blog - Nested</h1>
+    </div>
+  );
+}
