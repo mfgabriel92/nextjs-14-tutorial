6f9459df4ffe36e0078d79f499bb9a612afc199a
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-03-03 10:31:46 +0800

    feat: catch all segments

---
+++ src/app/documents/[...slug]/page.tsx
@@ -0,0 +1,21 @@
+interface Params {
+  params: {
+    slug: string[];
+  };
+}
+
+export default function Documents({ params }: Params) {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl">Documents</h1>
+      {params.slug.map((s) => (
+        <div
+          key={s}
+          className="text-gray-400"
+        >
+          <p className="text-xl">{s}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
