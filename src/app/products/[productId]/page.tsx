579e1ca53cffa1c136431ab0e1744d1769fc0df8
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-03-04 11:10:55 +0800

    feat: routing metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,28 @@
+import { Metadata } from "next";
+
 interface Params {
   params: {
     productId: number;
   };
 }
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const title = (await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Product #${params.productId}`);
+    }, 1500);
+  })) as string;
+
+  return {
+    title,
+  };
+}
+
+// export function generateMetadata({ params }: Params): Metadata {
+//   return {
+//     title: `Product #${params.productId}`,
+//   };
+// }
 
 export default function ProductDetails({ params }: Params) {
   return <h1 className="text-2xl">Product #{params.productId} details</h1>;
